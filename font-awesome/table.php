ce8ae6f52e5eff18cbbb415f16a2f3faca59a5e1
= <=>
2019-10-21 12:01:22 -0300

    modal

--- font-awesome/table.php
+++ font-awesome/table.php
@@ -8,6 +8,15 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<script>
+		$(document).ready(function () {
+			$('#tabla .btn-danger').on('click', function (e) {
+				e.preventDefault();
+				$('#ventana').modal('show');
+			});
+		});
+	</script>
 </head>
 <body>
 	<body>
@@ -55,42 +64,80 @@
 				</nav>
 			</div>
 			<div class="col-md-9">
-				<form action="?">
-					<div class="form-group">
-						<label for="nombre">Nombre</label>
-						<input class="form-control is-invalid" type="text">
-						<div class="valid-feedback">Todo salió bien</div>
-						<div class="invalid-feedback">Todo salió mal</div>
-					</div>
-
-					<div class="form-group">
-						<label for="jquery">Test con jQuery</label>
-						<input id="input-jquery" class="form-control" type="text">
-						<div class="valid-feedback">Todo salió bien</div>
-						<div class="invalid-feedback">Todo salió mal</div>
-					</div>
-
-					<p>Elegí una opción</p>
-					
-					<div class="form-check">
-						<input type="checkbox" name="opciones[]" class="form-check-input" id="opcion1">
-						<label for="opcion1" class="form-check-label">Opción 1</label>
-					</div>
-
-					<div class="form-check">
-						<input type="checkbox" name="opciones[]" class="form-check-input" id="opcion2">
-						<label for="opcion2" class="form-check-label">Opción 1</label>
-					</div>
-
-					<div class="form-check">
-						<input type="checkbox" name="opciones[]" class="form-check-input" id="opcion3">
-						<label for="opcion3" class="form-check-label">Opción 1</label>
-					</div>
-
-				</form>
+				<table id="tabla" class="table table-bordered table-striped">
+					<thead>
+						<tr>
+							<th>Nombre</th>
+							<th>Apellido</th>
+							<th>Acciones</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Lorem</td>
+							<td>Ipsum</td>
+							<td>
+								<a title="Editar" class="btn btn-primary" href="#">
+									<span class="fa fa-pencil"></span>
+								</a>
+								<a title="Borrar" class="btn btn-danger" href="/">
+									<span class="fa fa-trash"></span>
+								</a>
+							</td>
+						</tr>
+						<tr>
+							<td>Lorem</td>
+							<td>Ipsum</td>
+							<td>
+								<a title="Editar" class="btn btn-primary" href="#">
+									<span class="fa fa-pencil"></span>
+								</a>
+								<a title="Borrar" class="btn btn-danger" href="#">
+									<span class="fa fa-trash"></span>
+								</a>
+							</td>
+						</tr>
+						<tr>
+							<td>Lorem</td>
+							<td>Ipsum</td>
+							<td>
+								<a title="Editar" class="btn btn-primary" href="#">
+									<span class="fa fa-pencil"></span>
+								</a>
+								<a title="Borrar" class="btn btn-danger" href="#">
+									<span class="fa fa-trash"></span>
+								</a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 		</div>
 
+	</div>
+
+
+	<div id="ventana" class="modal" tabindex="-1" role="dialog">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title">Borrar</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        <p>¿Está seguro que desea borrar este elemento?</p>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-primary">
+	        	<span style="display: none" class="fa fa-spinner fa-spin"></span> 
+	        	Borrar
+	        </button>
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+	      </div>
+	    </div>
+	  </div>
 	</div>
 </body>
 </body>
