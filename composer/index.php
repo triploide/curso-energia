d3b9b69e32dbf61ab4406a8ecb4064dffbf78d3b
= <=>
2019-11-08 18:00:16 -0300

    ejercicios

--- composer/index.php
+++ composer/index.php
@@ -10,3 +10,14 @@ $hoy = Carbon\Carbon::createFromFormat('d-m-Y', '08-11-2019');
 
 var_dump($hoy->format('Y-m-d h:i:s'));
 
+// crear una nueva fecha con carbon now
+// sumarle 10 min a la fecha
+// mostrar la fecha con formato d-m-Y
+
+// crear una nueva fecha a partir a partir de este string 28-02-2019
+// sumarle 1 día a la fecha anterior
+// mostrar el número de mes de la fecha anterior
+
+
+
+
