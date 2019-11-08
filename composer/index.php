85363cb8c9c4356b2467d2d1a76912676eb8ad2c
= <=>
2019-11-08 19:10:08 -0300

    objetos

--- composer/index.php
+++ composer/index.php
@@ -2,9 +2,13 @@
 
 require 'vendor/autoload.php';
 
-$carbon = Carbon\Carbon::now();
+$carbon = Carbon\Carbon::now()->locale('es_ES')->format('jS F y');
+var_dump($carbon); exit;
 
 $carbon->addDays(10)->format('Y-m-d h:i:s');
+// o también
+// $carbon->addDays(10);
+// $carbon->format('Y-m-d h:i:s');
 
 $hoy = Carbon\Carbon::createFromFormat('d-m-Y', '08-11-2019');
 
@@ -17,7 +21,4 @@ var_dump($hoy->format('Y-m-d h:i:s'));
 // crear una nueva fecha a partir a partir de este string 28-02-2019
 // sumarle 1 día a la fecha anterior
 // mostrar el número de mes de la fecha anterior
-
-
-
 
