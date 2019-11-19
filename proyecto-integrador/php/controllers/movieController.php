e572a2fa95da386c1834b05de0f676671b71fbf2
= <=>
2019-11-19 19:06:06 -0300

    php.ini

--- proyecto-integrador/php/controllers/movieController.php
+++ proyecto-integrador/php/controllers/movieController.php
@@ -11,17 +11,12 @@ require_once '../classes/Movie.php';
 require_once '../classes/MySQLDB.php';
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-$image = Image::make($_FILES['banner']['tmp_name']);
+
+
 /*
 $image->resize(null, 100, function ($constraint) {
 	$constraint->aspectRatio();
 });*/
-
-$image->fit(900, 600);
-$image->insert($CONFIG['filesystem']['images'] . 'watermark.png', 'center');
-$nombre = uniqid() . '.jpg';
-$image->save($CONFIG['filesystem']['path'] . 'movies/' . $nombre);
-
 //$image->crop(100, 200);
 //$image->with(); // getter
 //$image->height(); // getter
@@ -32,7 +27,14 @@ $image->save($CONFIG['filesystem']['path'] . 'movies/' . $nombre);
 //Agregar la columna banner en la table movies
 //Persistir el nombre de imagen en la base
 
-exit;
+if (isset($_FILES['banner']) && $_FILES['banner']['error'] == UPLOAD_ERR_OK) {
+	$image = Image::make($_FILES['banner']['tmp_name']);
+	$image->fit(900, 600);
+	$image->insert($CONFIG['filesystem']['images'] . 'watermark.png', 'center');
+	$ext = pathinfo($_FILES['banner']['name'], PATHINFO_EXTENSION);
+	$banner = uniqid() . '.' . $ext;
+	$image->save($CONFIG['filesystem']['path'] . 'movies/' . $banner);
+}
 
 
 if ($_POST['id']) {
@@ -47,8 +49,7 @@ $movie->setColumna('title', $_POST['title']);
 $movie->setColumna('rating', $_POST['rating']);
 $movie->setColumna('length', $_POST['length']);
 $movie->setColumna('release_date', $fecha->format('Y-m-d'));
+$movie->setColumna('banner', $banner);
 $movie->save();
-
-
 
 //header('location: ../../movies');
