4e24808125833413b5eaf2f2572da429c1344aa8
= <=>
2019-11-26 19:00:00 -0300

    app

--- proyecto-integrador-lucho/php/cache/8eccf09f7b21e6145721b12deb79c07717cba748.php
+++ proyecto-integrador-lucho/php/cache/8eccf09f7b21e6145721b12deb79c07717cba748.php
@@ -2,10 +2,22 @@
 <html lang="es">
 	<head>
 		<meta charset="UTF-8">
-		<?php echo $__env->yieldContent('head'); ?>
 		<title><?php echo $__env->yieldContent('title'); ?></title>
+		<?php echo $__env->make('tpl/head', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+		<?php echo $__env->yieldContent('head'); ?>
 	</head>
 	<body>
-		<?php echo $__env->yieldContent('body'); ?>
+		<div class="container">
+			<?php echo $__env->make('tpl/nav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+			<main class="row">
+				<div class="col-md-3">
+					<?php echo $__env->make('tpl/menu', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+				</div>
+				<div class="col-md-9">
+					<?php echo $__env->yieldContent('content'); ?>
+				</div>
+			</main>
+		</div>
 	</body>
 </html><?php /**PATH C:\xampp\htdocs\curso-energia\proyecto-integrador-lucho\php\views/app.blade.php ENDPATH**/ ?>
--- proyecto-integrador-lucho/php/views/app.blade.php
+++ proyecto-integrador-lucho/php/views/app.blade.php
@@ -2,10 +2,22 @@
 <html lang="es">
 	<head>
 		<meta charset="UTF-8">
-		@yield('head')
 		<title>@yield('title')</title>
+		@include('tpl/head')
+		@yield('head')
 	</head>
 	<body>
-		@yield('body')
+		<div class="container">
+			@include('tpl/nav')
+
+			<main class="row">
+				<div class="col-md-3">
+					@include('tpl/menu')
+				</div>
+				<div class="col-md-9">
+					@yield('content')
+				</div>
+			</main>
+		</div>
 	</body>
 </html>
