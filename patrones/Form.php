b163f47872210a8b75b3d6c3f1eb9929a53016d1
= <=>
2019-12-03 18:37:39 -0300

    Clase Form

---
+++ patrones/Form.php
@@ -0,0 +1,22 @@
+<?php
+
+class Form
+{
+	public static function select($name, $options, $valorActual=null)
+	{
+		//if ($valorActual == $key) $selected = 'selected';
+
+		return '<select name="$name">foreach ($options as $key => $option) { <option value="$key" $selected>$value</option> } </select>';
+	}
+}
+
+$form = new Form;
+$form->accion();
+
+$options = [
+	1 => 'Acción',
+	2 => 'Aventura',
+	3 => 'Terror',
+]
+
+Form::select('genre_id', $options, $movie->genre_id)
